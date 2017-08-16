2c1e88a616b030d35ddee35c12978fdf458ca3aa
Niquo <niquo@mit.edu>
2017-08-16 16:08:21 +0300

    create credentials json obj function added

--- restAPI/tools/accessDB.py
+++ restAPI/tools/accessDB.py
@@ -9,6 +9,15 @@ HOST = 'MYSQL_DATABASE_HOST'
 def get_credentials(credentials_path=CREDENTIALS_PATH):
 	return load_data(credentials_path)
 
+def create_credentials(username, password, db, host, credentials_path=CREDENTIALS_PATH):
+	credentials_obj = {}
+	credentials_obj[USER] = username
+	credentials_obj[PASSWORD] = password
+	credentials_obj[DATABASE] = db
+	credentials_obj[HOST] = host
+	return dump_data(credentials_path, credentials_obj)
+
+
 def load_data(data_path):
 	with open(data_path, 'r') as infile:
 		data = json.load(infile)
